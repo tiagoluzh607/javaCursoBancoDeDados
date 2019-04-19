e1c36d81a124574e4a39154c48628bf0421694f4
tiagoluz <tiagoluz.h607@gmail.com>
2019-04-19 19:29:05 -0300

    Aula 06

---
+++ src/dao/UsuarioDAO.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Usuario;
+import view.FormCadastroView;
+
+/**
+ *
+ * @author tiago
+ */
+public class UsuarioDAO {
+    
+    private final Connection connection;
+
+    public UsuarioDAO(Connection connection) {
+        this.connection = connection;
+    }
+    
+    public void insert(Usuario usuario) throws SQLException{
+   
+            String sql = "insert into usuario(usuario,senha) values('"+usuario.getUsuario()+"','"+usuario.getSenha()+"'); ";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.execute();        
+            connection.close();
+            
+    }
+    
+    
+}
